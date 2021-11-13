616c21fed83e7ce83691fdfc6dee1be1a4f24cf2
Will Cohen <chewnoill@gmail.com>
2021-11-13 08:26:39 -0500

    updates

---
+++ webpack.run-dev.js
@@ -0,0 +1,53 @@
+    const webpackConfig = [{
+      mode: "development",
+      output: {
+        library: 'docLoader',
+        libraryTarget: 'umd',
+        globalObject: 'this',
+      },
+      externals: {
+        'react': 'React',
+        'react-dom': 'ReactDOM',
+      },
+      resolve: {
+      },
+      resolveLoader: {
+        modules: [resolveModules],
+      },
+      entry: {
+        ...files.reduce((acc, filename) => ({
+          ...acc,
+          [selectEntrypoint(filename)]: './' + folder + '/' + filename,
+        }), {}),
+      },
+      devServer: {},
+      module: {
+        rules: [
+          {
+            test: /\.mdx?$/,
+            use: [
+              {
+                loader: "babel-loader",
+                options: {
+                  presets: ["@babel/preset-react"].map(require.resolve as any)
+                }
+              },
+              {
+                loader: '@mdx-js/loader',
+                options: {
+                  renderer: ``
+                },
+              },
+            ]
+          }
+        ]
+      },
+      plugins: [...files.map(filename => new HtmlWebpackPlugin({
+        inject: 'head',
+        scriptLoading: 'blocking',
+        chunks: [selectEntrypoint(filename)],
+        filename: selectEntrypoint(filename) + ".html",
+        templateContent: buildDynamicHTML()
+      })),
+      ],
+    }];

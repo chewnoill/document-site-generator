06230c7d4931eeb714a3a244ffdc551b95dda6ef
Will Cohen <chewnoill@gmail.com>
2022-02-22 09:08:04 -0500

    [chore] size stl p tag
    something is wrong with sizing divs?

--- src/main/components/stl.jsx
+++ src/main/components/stl.jsx
@@ -1,15 +1,20 @@
+import styled from "@emotion/styled";
 import { StlViewer } from "react-stl-viewer";
+
+const Wrapper = styled.p(
+  ({ size }) => `
+  height:${size}px;
+  width:${size}px;
+`);
 
 export default function Stl({ url, size = 500 }) {
   return (
-    <StlViewer
-      style={{
-        width: `${size}px`,
-        height: `${size}px`,
-      }}
-      orbitControls
-      shadows
-      url={url}
-    />
+    <Wrapper size={size}>
+      <StlViewer
+        orbitControls
+        shadows
+        url={url}
+      />
+    </Wrapper>
   );
 }
